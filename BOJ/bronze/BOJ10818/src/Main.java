06a24909286d57ecc7e63e8182aeb7c6475a58f9
dbstnr2146 <dbstnr2146@naver.com>
2022-08-11 22:10:56 +0900

    0811

---
+++ BOJ/bronze/BOJ10818/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N=scanner.nextInt();
+        int [] numbers= new int[N];
+        for (int i=0; i<N;i++){
+            numbers[i]=scanner.nextInt();
+        }
+        scanner.close();
+        Arrays.sort(numbers);
+        System.out.print(numbers[0]+" "+ numbers[N-1]);
+
+
+    }
+}
