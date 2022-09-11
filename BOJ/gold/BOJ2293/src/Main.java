07ee06fb8f6dd4f418f440b4a936bf21f23ed1fe
dbstnr2146 <dbstnr2146@naver.com>
2022-09-11 23:37:51 +0900

    0911

--- BOJ/gold/BOJ2293/src/Main.java
+++ BOJ/gold/BOJ2293/src/Main.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //   0,1,2,3,4,5,6,7,8,9,10
-        // 1 0,1,1,1,1,1,1,1,1,1,1
-        // 2 0,0 1,1
-        // 3 0 0 0 2
+//      0 1 2 3 4 5 6 7 8 9 10
+//  1   1 1 1 1 1 1 1 1 1 1 1
+//  2   0 0 1 1 2 2 3 3 4 4 5
+//  5   0 0 0 0 0 1 1 2 2 3 4
+// ===========================
+//      1 1 2 2 3 4 5 6 7 8 10
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
