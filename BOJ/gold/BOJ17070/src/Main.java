b59a0d021a3f3f2a863b52d382555c6e5b6c0a9c
dbstnr2146 <dbstnr2146@naver.com>
2022-09-23 00:52:38 +0900

    0922

--- BOJ/gold/BOJ17070/src/Main.java
+++ BOJ/gold/BOJ17070/src/Main.java
@@ -7,11 +7,13 @@ import java.util.LinkedList;
 public class Main {
     static int N;
     static int count;
+    static int[][] state;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        int[][] state = new int[N][N];
+        state = new int[N][N];
         int[][] visited = new int[N][N];
 
         for (int i = 0; i < N; i++) {
@@ -20,28 +22,46 @@ public class Main {
                 state[i][j] = Integer.parseInt(s[j]);
             }
         }
-        DFS(0,1,0);
-
+        DFS(0, 1, 0);
+        System.out.println(count);
 
 
     }
 
     private static void DFS(int col, int row, int direction) {
-        if (col==N-1 && col==N-1){
+        if (col == N - 1 && row == N - 1) {
             count++;
             return;
         }
-        if (direction==0){
-            if (row==N-1){
-                return;
+        if (direction == 0) {
+
+            if (row + 1 < N && state[col][row + 1] == 0) {
+                DFS(col, row + 1, 0);
+            }
+            if (row + 1 < N && col + 1 < N && state[col + 1][row + 1] == 0 && state[col][row + 1] == 0 && state[col + 1][row] == 0) {
+                DFS(col + 1, row + 1, 2);
             }
 
 
-        }
-        else if (direction==1){
+        } else if (direction == 1) {
 
-        }
-        else if (direction==2){
+            if (col + 1 < N && state[col + 1][row] == 0) {
+                DFS(col + 1, row, 1);
+            }
+            if (row + 1 < N && col + 1 < N && state[col + 1][row + 1] == 0 && state[col][row + 1] == 0 && state[col + 1][row] == 0) {
+                DFS(col + 1, row + 1, 2);
+            }
+
+        } else if (direction == 2) {
+            if (row + 1 < N && state[col][row + 1] == 0) {
+                DFS(col, row + 1, 0);
+            }
+            if (col + 1 < N && state[col + 1][row] == 0) {
+                DFS(col + 1, row, 1);
+            }
+            if (row + 1 < N && col + 1 < N && state[col + 1][row + 1] == 0 && state[col][row + 1] == 0 && state[col + 1][row] == 0) {
+                DFS(col + 1, row + 1, 2);
+            }
 
         }
     }
