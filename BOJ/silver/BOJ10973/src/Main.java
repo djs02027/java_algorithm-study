8b61a53c95778b4de2baeae30b12e24ccea36c94
djs02027 <djs02027@gmail.com>
2022-10-24 23:06:52 +0900

    1024

--- BOJ/silver/BOJ10973/src/Main.java
+++ BOJ/silver/BOJ10973/src/Main.java
@@ -24,42 +24,42 @@ public class Main {
 
     }
 
-    private static void perm(int[] visited, int[] find, int[] num, int n, int r, int count) {
-        if(n==r){
-            result.add(find.clone());
-            int same=0;
-            for (int i = 0; i < find.length; i++) {
-                if(find[i]==num[i]){
-                    same+=1;
-                }
-            }
-
-            if(same==n){
-                sameIdx=step;
-                if (sameIdx==0){
-                    System.out.println(-1);
-                }else{
-
-                    int [] tmp = result.get((sameIdx-1));
-                    StringBuilder sb =new StringBuilder();
-                    for (int i = 0; i < tmp.length; i++) {
-                        sb.append(tmp[i]+" ");
-                    }
-                    System.out.println(sb.toString());
-                }
-                System.exit(0);
-            }
-            step+=1;
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if(visited[i]==0){
-                visited[i]=1;
-                find[r]=i+1;
-                perm(visited, find, num, n, r+1, count+1);
-                visited[i]=0;
-            }
-
-        }
-    }
+//    private static void perm(int[] visited, int[] find, int[] num, int n, int r, int count) {
+//        if(n==r){
+//            result.add(find.clone());
+//            int same=0;
+//            for (int i = 0; i < find.length; i++) {
+//                if(find[i]==num[i]){
+//                    same+=1;
+//                }
+//            }
+//
+//            if(same==n){
+//                sameIdx=step;
+//                if (sameIdx==0){
+//                    System.out.println(-1);
+//                }else{
+//
+//                    int [] tmp = result.get((sameIdx-1));
+//                    StringBuilder sb =new StringBuilder();
+//                    for (int i = 0; i < tmp.length; i++) {
+//                        sb.append(tmp[i]+" ");
+//                    }
+//                    System.out.println(sb.toString());
+//                }
+//                System.exit(0);
+//            }
+//            step+=1;
+//            return;
+//        }
+//        for (int i = 0; i < n; i++) {
+//            if(visited[i]==0){
+//                visited[i]=1;
+//                find[r]=i+1;
+//                perm(visited, find, num, n, r+1, count+1);
+//                visited[i]=0;
+//            }
+//
+//        }
+//}
 }
