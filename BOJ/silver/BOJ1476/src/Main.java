2cebb6cde866c51afc2cf398802493299ff5c839
djs02027 <djs02027@gmail.com>
2022-11-16 16:10:25 +0900

    1116

--- BOJ/silver/BOJ1476/src/Main.java
+++ BOJ/silver/BOJ1476/src/Main.java
@@ -6,25 +6,21 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine());
-        int E=Integer.parseInt(st.nextToken());
-        int S=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
-
-        int E_MAX=15;
-        int S_MAX=28;
-        int M_MAX=19;
-
-
-        int cacYear=E;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int e = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int E_MAX = 15;
+        int S_MAX = 28;
+        int M_MAX = 19;
+        int year=e;
         while (true){
-            if((cacYear-S)%S_MAX==0 &&(cacYear-M)%M_MAX==0 ){
+            if((year-s)%S_MAX ==0 && (year-m)%M_MAX==0){
                 break;
             }
-            cacYear+=E_MAX;
+                year+=E_MAX;
         }
-        System.out.println(cacYear);
-
+        System.out.println(year);
 
     }
 
